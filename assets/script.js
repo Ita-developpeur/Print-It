fd6a295e2886c7b43709c19602de9ac293424a70
Florian <herrick.florian.29@gmail.com>
2023-09-18 11:14:31 +0200

    Update print-it

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+"use strict";
+
+// ******************** CONSTANTS ********************
+
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -15,48 +19,71 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
 
+const arrowLeft   = document.querySelector(".arrow_left");
+const arrowRight  = document.querySelector(".arrow_right");
+const dots        = document.querySelector(".dots");
+const banner      = document.querySelector(".banner-img");
+const tagLine     = document.querySelector("#banner > p");
 
-const arrowLeft = document.querySelector(".arrow_left");
-const arrowRight = document.querySelector(".arrow_right");
-const dots = document.querySelector(".dots");
-let banner = document.querySelector(".banner-img");
-let subtitle = document.querySelector("#banner > p");
-let counter = 0;
+// ******************** VARIABLES ********************
+
+let counter   = 0;
 let imgLength = slides.length;
 
+// ******************** FUNCTIONS ********************
+
+/**
+ * Moves the slide to the left in the slideshow.
+ */
+function goLeft() {
+  dot[counter].classList.remove("dot_selected");
+  counter--;
+
+  if (counter === -1) {
+    counter = imgLength - 1;
+  }
+
+  banner.src = "./assets/images/slideshow/" + slides[counter].image;
+  tagLine.innerHTML = slides[counter].tagLine;
+  dot[counter].classList.add("dot_selected");
+}
+
+/**
+ * Moves the slideshow to the next image on the right.
+ */
+function goRight() {
+  dot[counter].classList.remove("dot_selected");
+  counter++;
+
+  if (counter === imgLength) {
+    counter = 0;
+  }
+
+  banner.src = "./assets/images/slideshow/" + slides[counter].image;
+  tagLine.innerHTML = slides[counter].tagLine;
+  dot[counter].classList.add("dot_selected");
+}
+
+/**
+ * Runs the slider by setting the initial image and tagline, and adding event listeners for arrow buttons.
+ */
+function setSlider() {
+  banner.src = "./assets/images/slideshow/" + slides[0].image;
+  tagLine.innerHTML = slides[0].tagLine;
+  dot[0].classList.add("dot_selected");
+  
+  arrowLeft.addEventListener("click", goLeft);
+  arrowRight.addEventListener("click", goRight);
+}
+
+// ******************** MAIN ********************
+
 for (let i = 0; i < imgLength; i++) {
-dots.innerHTML = dots.innerHTML + "<span class='dot'></span>";
+  dots.innerHTML = dots.innerHTML + "<span class='dot'></span>";
 }
-let dot = document.querySelectorAll(".dot");
 
-banner.src = "./assets/images/slideshow/" + slides[0].image;
-subtitle.innerHTML = slides[0].tagLine;
-dot[0].classList.add("dot_selected");
+const dot = document.querySelectorAll(".dot");
 
-
-arrowLeft.addEventListener("click", () => {
-dot[counter].classList.remove("dot_selected");
-counter--;
-if (counter === -1) {
-counter = imgLength - 1;
-}
-banner.src = "./assets/images/slideshow/" + slides[counter].image;
-subtitle.innerHTML = slides[counter].tagLine;
-dot[counter].classList.add("dot_selected");
-console.log("Click Left, ", counter);
-});
-
-
-arrowRight.addEventListener("click", () => {
-dot[counter].classList.remove("dot_selected");
-counter++;
-if (counter === imgLength) {
-counter = 0;
-}
-banner.src = "./assets/images/slideshow/" + slides[counter].image;
-subtitle.innerHTML = slides[counter].tagLine;
-dot[counter].classList.add("dot_selected");
-console.log("Click Right, ", counter);
-});
+setSlider();
